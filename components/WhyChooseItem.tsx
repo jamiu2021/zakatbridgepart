62d723f4225197e81baee278bb17e3d70983b47e
jamiu2021 <jamiu.olaide1991@gmail.com>
2025-05-04 22:03:12 -0700

    little correction

--- components/WhyChooseItem.tsx
+++ components/WhyChooseItem.tsx
@@ -8,11 +8,11 @@ type WhyChooseItemProps = {
 
 const WhyChooseItem = ({ icon: Icon, title, content }: WhyChooseItemProps) => {
   return (
-    <div className='bg-[#ffff] px-6 py-5 rounded-2xl shadow-md shadow-[#D9D9D9]'>
+    <div className='bg-[#ffff] p-6 rounded-2xl shadow-md shadow-[#D9D9D9]'>
       
         <Icon className='text-4xl text-[#0098CC]' />
       
-      <h2 className="font-bold mt-4 mb-4">{title}</h2>
+      <h2 className="font-bold py-4">{title}</h2>
       <p className="text-base">{content}</p>
     </div>
   );
